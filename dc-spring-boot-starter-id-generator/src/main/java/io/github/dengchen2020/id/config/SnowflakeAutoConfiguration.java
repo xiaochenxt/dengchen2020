cda998ca9f7a5098c4736ec49eeaf1130d8c1902
xc <xc@>
2025-10-19 16:00:02 +0800

    优化

--- dc-spring-boot-starter-id-generator/src/main/java/io/github/dengchen2020/id/config/SnowflakeAutoConfiguration.java
+++ dc-spring-boot-starter-id-generator/src/main/java/io/github/dengchen2020/id/config/SnowflakeAutoConfiguration.java
@@ -2,11 +2,8 @@ package io.github.dengchen2020.id.config;
 
 import io.github.dengchen2020.id.properties.IdGeneratorBuilder;
 import io.github.dengchen2020.id.snowflake.SnowflakeSmartLifecycle;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -24,14 +21,12 @@ import org.springframework.data.redis.core.StringRedisTemplate;
  * @author xiaochen
  * @since 2024/7/2
  */
-@ConditionalOnClass(RedisConnectionFactory.class)
+@ConditionalOnBean(RedisConnectionFactory.class)
 @EnableConfigurationProperties(IdGeneratorBuilder.class)
 @ConditionalOnProperty(value = "dc.id.type",matchIfMissing = true,havingValue = "snowflake")
-@AutoConfigureAfter(RedisAutoConfiguration.class)
 @Configuration(proxyBeanMethods = false)
 public class SnowflakeAutoConfiguration {
 
-    @ConditionalOnBean(RedisConnectionFactory.class)
     @Bean
     public SnowflakeSmartLifecycle snowflakeSmartLifecycle(RedisConnectionFactory redisConnectionFactory, IdGeneratorBuilder idGeneratorBuilder){
         return new SnowflakeSmartLifecycle(new StringRedisTemplate(redisConnectionFactory),idGeneratorBuilder.getSnowflake());
