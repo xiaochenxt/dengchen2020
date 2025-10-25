5aff893b9023f6962029f28eb86487e5545d4368
xc <xc@>
2025-10-25 13:16:33 +0800

    优化

---
+++ dc-spring-boot-starter-message/src/main/java/io/github/dengchen2020/message/config/EmailClientAutoConfiguration.java
@@ -0,0 +1,33 @@
+package io.github.dengchen2020.message.config;
+
+import io.github.dengchen2020.message.email.EmailClient;
+import io.github.dengchen2020.message.email.EmailClientImpl;
+import io.github.dengchen2020.message.properties.DcMessageBuilder;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+
+/**
+ * 电子邮件发送客户端自动配置
+ * @author xiaochen
+ * @since 2025/10/24
+ */
+@AutoConfigureAfter(MailSenderAutoConfiguration.class)
+@ConditionalOnBean(JavaMailSender.class)
+@ConditionalOnClass(MimeMessage.class)
+@Configuration(proxyBeanMethods = false)
+public final class EmailClientAutoConfiguration {
+
+    @ConditionalOnMissingBean
+    @Bean
+    EmailClient emailService(JavaMailSender javaMailSender, DcMessageBuilder dcMessageBuilder){
+        return new EmailClientImpl(javaMailSender, dcMessageBuilder.getEmail().getTo());
+    }
+
+}
--- dc-spring-boot-starter-message/src/main/java/io/github/dengchen2020/message/config/MessageAutoConfiguration.java
+++ dc-spring-boot-starter-message/src/main/java/io/github/dengchen2020/message/config/MessageAutoConfiguration.java
@@ -2,24 +2,18 @@ package io.github.dengchen2020.message.config;
 
 import io.github.dengchen2020.message.dingtalk.DingTalkClient;
 import io.github.dengchen2020.message.dingtalk.DingTalkClientImpl;
-import io.github.dengchen2020.message.email.EmailClient;
-import io.github.dengchen2020.message.email.EmailClientImpl;
 import io.github.dengchen2020.message.feishu.FeiShuClient;
 import io.github.dengchen2020.message.feishu.FeiShuClientImpl;
 import io.github.dengchen2020.message.properties.DcMessageBuilder;
 import io.github.dengchen2020.message.wechat.WeChatClient;
 import io.github.dengchen2020.message.wechat.WeChatClientImpl;
-import jakarta.mail.internet.MimeMessage;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
 
 /**
- * 消息自动配置
+ * 消息发送客户端自动配置
  * @author xiaochen
  * @since 2024/6/4
  */
@@ -55,16 +49,6 @@ public final class MessageAutoConfiguration {
     @Bean
     WeChatClient weChatClient(){
         return new WeChatClientImpl(weChat.getWebhook());
-    }
-
-    @ConditionalOnClass(MimeMessage.class)
-    @Configuration(proxyBeanMethods = false)
-    static final class EmailClientAutoConfiguration {
-        @ConditionalOnBean(JavaMailSender.class)
-        @Bean
-        EmailClient emailService(JavaMailSender javaMailSender, DcMessageBuilder dcMessageBuilder){
-            return new EmailClientImpl(javaMailSender, dcMessageBuilder.getEmail().getTo());
-        }
     }
 
 }
