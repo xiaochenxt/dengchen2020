920df32a1b75ac77bc0902d60f6200b320012674
xc <xc@>
2025-11-20 10:58:05 +0800

    优化

--- dc-utils/src/main/java/io/github/dengchen2020/core/utils/StrUtils.java
+++ dc-utils/src/main/java/io/github/dengchen2020/core/utils/StrUtils.java
@@ -1,6 +1,6 @@
 package io.github.dengchen2020.core.utils;
 
-import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.NullMarked;
 import org.jspecify.annotations.Nullable;
 import org.slf4j.helpers.MessageFormatter;
 
@@ -14,6 +14,7 @@ import java.util.UUID;
  * @author xiaochen
  * @since 2022/8/19
  */
+@NullMarked
 public abstract class StrUtils {
 
     /**
@@ -22,7 +23,7 @@ public abstract class StrUtils {
      * @param args 参数
      * @return 格式化后的字符串
      */
-    public static String format(@NonNull String strPattern,Object... args){
+    public static String format(String strPattern, Object... args){
         return MessageFormatter.arrayFormat(strPattern,args).getMessage();
     }
 
@@ -31,7 +32,7 @@ public abstract class StrUtils {
      * @param obj 对象
      * @return true | false
      */
-    private static boolean isArray(Object obj) {
+    private static boolean isArray(@Nullable Object obj) {
         return null != obj && obj.getClass().isArray();
     }
 
@@ -68,7 +69,8 @@ public abstract class StrUtils {
      * @param key 要查询的键
      * @return key对应的value，如果不存在则返回null
      */
-    public static String getValue(String query, String key) {
+    @Nullable
+    public static String getValue(@Nullable String query,@Nullable String key) {
         if (query == null || key == null || query.isEmpty() || key.isEmpty()) return null;
         final int keyLen = key.length();
         final int queryLen = query.length();
@@ -102,7 +104,7 @@ public abstract class StrUtils {
      * @param targetByteLength 目标字节数
      * @return 缩短字节后的字符串
      */
-    public static String shortenString(@NonNull String str, int targetByteLength) {
+    public static String shortenString(String str, int targetByteLength) {
         try {
             byte[] bytes = str.getBytes();
             int originalByteLength = bytes.length;
@@ -128,7 +130,7 @@ public abstract class StrUtils {
      * @param name 原字段名
      * @return 新字段名
      */
-    public static String convertToSnakeCase(@NonNull String name) {
+    public static String convertToSnakeCase(String name) {
         StringBuilder result = new StringBuilder();
         for (int i = 0,len = name.length(); i < len; i++) {
             char c = name.charAt(i);
@@ -160,7 +162,7 @@ public abstract class StrUtils {
      * @param cs  要检查的 CharSequence，可能为 null
      * @return {@code true} 如果仅包含数字，并且为非空值
      */
-    public static boolean isNumeric(final @NonNull CharSequence cs) {
+    public static boolean isNumeric(final CharSequence cs) {
         if (!hasText(cs)) return false;
         final int sz = cs.length();
         for (int i = 0; i < sz; i++) {
@@ -173,7 +175,7 @@ public abstract class StrUtils {
      * 将map转化为query参数，例如{@code a=1&b=2&c=3}
      * @param params map
      */
-    public static String toQuery(@NonNull Map<String, ?> params) {
+    public static String toQuery(Map<String, ?> params) {
         StringBuilder query = new StringBuilder();
         for (Iterator<? extends Map.Entry<String, ?>> iterator = params.entrySet().iterator(); iterator.hasNext(); ) {
             Map.Entry<String, ?> entry = iterator.next();
@@ -200,7 +202,7 @@ public abstract class StrUtils {
      * @param i 索引
      * @return 值
      */
-    public static String getValue(String @NonNull[] arr, int i) {
+    public static String getValue(String[] arr, int i) {
         if (arr.length >= i + 1) return arr[i];
         return "";
     }
