592d1577862de0a331dbb27d30f4fcd90233bc6e
xc <xc@>
2025-12-23 20:11:03 +0800

    优化

---
+++ dc-utils/src/main/java/io/github/dengchen2020/core/utils/spring/SpringUtils.java
@@ -0,0 +1,32 @@
+package io.github.dengchen2020.core.utils.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+
+/**
+ * Spring工具类，谨慎使用，容易导致将启动期的错误延迟到运行时
+ * @author xiaochen
+ * @since 2025/12/23
+ */
+public abstract class SpringUtils {
+
+    static ApplicationContext applicationContext;
+
+    public static ApplicationContext getApplicationContext() {
+        if (applicationContext == null) throw new IllegalStateException("Spring应用上下文未初始化");
+        return applicationContext;
+    }
+
+    public static Environment getEnvironment() {
+        return applicationContext.getEnvironment();
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        return applicationContext.getBean(clazz);
+    }
+
+    public static void publishEvent(Object event) {
+        applicationContext.publishEvent(event);
+    }
+
+}
