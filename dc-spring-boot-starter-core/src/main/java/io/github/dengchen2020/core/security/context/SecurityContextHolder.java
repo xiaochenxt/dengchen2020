f08c8c26dfc203c7efc4fc37b7bdcea88ab4575d
xc <xc@>
2026-01-21 10:57:03 +0800

    移除不常用功能

--- dc-spring-boot-starter-core/src/main/java/io/github/dengchen2020/core/security/context/SecurityContextHolder.java
+++ dc-spring-boot-starter-core/src/main/java/io/github/dengchen2020/core/security/context/SecurityContextHolder.java
@@ -1,11 +1,6 @@
 package io.github.dengchen2020.core.security.context;
 
 import io.github.dengchen2020.core.security.principal.Authentication;
-import org.jspecify.annotations.NonNull;
-import org.jspecify.annotations.Nullable;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 认证信息上下文管理
@@ -15,8 +10,6 @@ import java.util.Map;
 public class SecurityContextHolder {
 
     private final static ThreadLocal<Authentication> currentAuthentication = new ThreadLocal<>();
-
-    private final static ThreadLocal<Map<String, Object>> resources = new ThreadLocal<>();
 
     /**
      * 保存认证信息
@@ -43,58 +36,6 @@ public class SecurityContextHolder {
      */
     public static void clear() {
         currentAuthentication.remove();
-        resources.remove();
-    }
-
-    /**
-     * 获取资源
-     * @param key 资源key
-     * @return 资源
-     */
-    @Nullable
-    public static Object getResource(String key) {
-        if (key == null) return null;
-        Map<String, Object> map = resources.get();
-        if (map == null) return null;
-        return map.get(key);
-    }
-
-    /**
-     * 获取资源
-     * @param key 资源key
-     * @param defaultValue 资源不存在时返回默认值
-     * @return 资源
-     */
-    @Nullable
-    public static Object getResource(String key, Object defaultValue) {
-        if (key == null) return defaultValue;
-        Map<String, Object> map = resources.get();
-        if (map == null) return defaultValue;
-        return map.getOrDefault(key, defaultValue);
-    }
-
-    /**
-     * 绑定资源
-     * @param key 资源key
-     * @param value 资源
-     */
-    public static void bindResource(@NonNull String key, Object value) {
-        Map<String, Object> map = resources.get();
-        if (map == null) {
-            map = new HashMap<>();
-            resources.set(map);
-        }
-        map.put(key, value);
-    }
-
-    /**
-     * 解绑资源
-     * @param key 资源key
-     */
-    public static void unbindResource(@NonNull String key) {
-        Map<String, Object> map = resources.get();
-        if (map == null) return;
-        map.remove(key);
     }
 
 }
