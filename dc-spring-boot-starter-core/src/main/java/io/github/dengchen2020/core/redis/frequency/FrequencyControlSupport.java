30013324e2d8072535cc091acf33958d76055f3c
xc <xc@>
2026-01-21 17:41:15 +0800

    适配redis集群

--- dc-spring-boot-starter-core/src/main/java/io/github/dengchen2020/core/redis/frequency/FrequencyControlSupport.java
+++ dc-spring-boot-starter-core/src/main/java/io/github/dengchen2020/core/redis/frequency/FrequencyControlSupport.java
@@ -4,7 +4,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.core.script.RedisScript;
 
-import java.util.Collections;
+import java.util.List;
 
 /**
  * 频控
@@ -19,12 +19,12 @@ public class FrequencyControlSupport {
 
     RedisScript<Long> script = new DefaultRedisScript<>(
             """ 
-                    local qpsKey = "%s" .. ARGV[1]
-                    local qpmKey = "%s" .. ARGV[1]
-                    local qpdKey = "%s" .. ARGV[1]
-                    local qpsNum = tonumber(ARGV[2])
-                    local qpmNum = tonumber(ARGV[3])
-                    local qpdNum = tonumber(ARGV[4])
+                    local qpsKey = KEYS[1]
+                    local qpmKey = KEYS[2]
+                    local qpdKey = KEYS[3]
+                    local qpsNum = tonumber(ARGV[1])
+                    local qpmNum = tonumber(ARGV[2])
+                    local qpdNum = tonumber(ARGV[3])
                     if qpdNum > 0 then
                         local qpdValue = redis.call("GET", qpdKey)
                         if qpdValue then
@@ -68,7 +68,7 @@ public class FrequencyControlSupport {
                         redis.call("INCR", qpsKey)
                     end
                     return 0
-                    """.formatted(FREQUENCY_CONTROL_PREFIX+"qps:", FREQUENCY_CONTROL_PREFIX+"qpm:", FREQUENCY_CONTROL_PREFIX+"qpd:"),
+                    """,
             Long.class
     );
 
@@ -81,7 +81,8 @@ public class FrequencyControlSupport {
      * @return
      */
     public long trigger(String key, int qps, int qpm, int qpd) {
-        return redisTemplate.execute(script, Collections.emptyList(), key, String.valueOf(qps), String.valueOf(qpm), String.valueOf(qpd));
+        var slot = "{"+key+"}";
+        return redisTemplate.execute(script, List.of(FREQUENCY_CONTROL_PREFIX+"qps:"+slot, FREQUENCY_CONTROL_PREFIX+"qpm:"+slot, FREQUENCY_CONTROL_PREFIX+"qpd:"+slot), String.valueOf(qps), String.valueOf(qpm), String.valueOf(qpd));
     }
 
 }
