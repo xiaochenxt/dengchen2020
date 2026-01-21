728cc497a19f62f2f03762fa6fbe923db01a342a
xc <xc@>
2026-01-21 10:56:39 +0800

    优化

--- dc-spring-boot-starter-core/src/main/java/io/github/dengchen2020/core/properties/DcCorsProperties.java
+++ dc-spring-boot-starter-core/src/main/java/io/github/dengchen2020/core/properties/DcCorsProperties.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class DcCorsProperties {
 
     /**
-     * 是否启用CORS过滤器，默认启用，非特殊需求不建议关闭
+     * 是否启用CORS过滤器，默认启用，适用于前后端分离项目，非特殊需求不建议关闭
      */
     private boolean enabled = true;
 
