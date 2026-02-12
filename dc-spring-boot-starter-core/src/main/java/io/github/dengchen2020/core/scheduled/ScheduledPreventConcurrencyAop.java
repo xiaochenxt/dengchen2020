ebb71d3ad1566e384407a4c59f10afb612c0fbd0
xc <xc@>
2026-02-12 17:47:23 +0800

    优化

--- dc-spring-boot-starter-core/src/main/java/io/github/dengchen2020/core/scheduled/ScheduledPreventConcurrencyAop.java
+++ dc-spring-boot-starter-core/src/main/java/io/github/dengchen2020/core/scheduled/ScheduledPreventConcurrencyAop.java
@@ -106,10 +106,15 @@ public class ScheduledPreventConcurrencyAop implements SmartLifecycle {
 
     private static final RedisScript<Void> stopScript = new DefaultRedisScript<>("""
             local uniqueId = ARGV[1]
-            for _, key in ipairs(KEYS) do
-                if redis.call('get', key) == uniqueId then
-                    redis.call('del', key)
+            local values = redis.call('MGET', unpack(KEYS))
+            local keysToDel = {}
+            for i, val in ipairs(values) do
+                if val == uniqueId then
+                    table.insert(keysToDel, KEYS[i])
                 end
+            end
+            if #keysToDel > 0 then
+                redis.call('UNLINK', unpack(keysToDel))
             end
             """, Void.class);
 
