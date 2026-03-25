d6d0b55148299f1cb49cd7c8f612f09462466026
xc <xc@>
2026-03-25 13:57:06 +0800

    简化代码

--- jackson-ext/src/main/java/io/github/dengchen2020/jackson/DcModule.java
+++ jackson-ext/src/main/java/io/github/dengchen2020/jackson/DcModule.java
@@ -1,24 +1,13 @@
 package io.github.dengchen2020.jackson;
 
-import tools.jackson.core.Version;
-import tools.jackson.databind.JacksonModule;
+import tools.jackson.databind.module.SimpleModule;
 
 /**
- * 全局模块
+ * Jackson模块-dc
  * @author xiaochen
  * @since 2026/3/23
  */
-public class DcModule extends JacksonModule {
-
-    @Override
-    public String getModuleName() {
-        return "dc";
-    }
-
-    @Override
-    public Version version() {
-        return Version.unknownVersion();
-    }
+public class DcModule extends SimpleModule {
 
     @Override
     public void setupModule(SetupContext context) {
