be1666645a5e1828fe8faf9b074aea07a910cf5b
xc <xc@>
2026-04-18 09:06:51 +0800

    优化

--- dc-spring-boot-starter-jpa/src/main/java/io/github/dengchen2020/jpa/config/JpaAutoConfiguration.java
+++ dc-spring-boot-starter-jpa/src/main/java/io/github/dengchen2020/jpa/config/JpaAutoConfiguration.java
@@ -1,10 +1,10 @@
 package io.github.dengchen2020.jpa.config;
 
 import org.hibernate.cfg.AvailableSettings;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.hibernate.autoconfigure.HibernatePropertiesCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 /**
  * jpa自动配置
@@ -15,13 +15,20 @@ import org.springframework.context.annotation.Configuration;
 public final class JpaAutoConfiguration {
 
     @Bean
-    HibernatePropertiesCustomizer dcHibernatePropertiesCustomizer(@Value("${spring.profiles.active:null}") String active) {
+    HibernatePropertiesCustomizer dcHibernatePropertiesCustomizer(Environment environment) {
         return hibernateProperties -> {
-            if (!"dev".equals(active)) {
+            var active = environment.getActiveProfiles();
+            if (active.length != 1 || !"dev".equals(active[0])) { // 如果只启用了一个环境配置并且是开发环境，此时才允许自动建表
                 // 禁用自动建表
                 hibernateProperties.remove(AvailableSettings.HBM2DDL_AUTO);
                 // 禁用启动时检查命名查询
                 hibernateProperties.put("hibernate.query.startup_check", false);
+            }
+            if (environment.getProperty("dc.jpa.properties.hibernate.dynamic-insert", Boolean.class, true)) {
+                hibernateProperties.put("dc.hibernate.dynamic_insert", true);
+            }
+            if (environment.getProperty("dc.jpa.properties.hibernate.dynamic-update", Boolean.class, true)) {
+                hibernateProperties.put("dc.hibernate.dynamic_update", true);
             }
         };
     }
