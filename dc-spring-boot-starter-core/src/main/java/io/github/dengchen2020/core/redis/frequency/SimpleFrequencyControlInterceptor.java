55c492df5767f0d40c4f904dfa459374ea0572b2
xc <xc@>
2026-05-04 17:54:59 +0800

    优化频控说明

--- dc-spring-boot-starter-core/src/main/java/io/github/dengchen2020/core/redis/frequency/SimpleFrequencyControlInterceptor.java
+++ dc-spring-boot-starter-core/src/main/java/io/github/dengchen2020/core/redis/frequency/SimpleFrequencyControlInterceptor.java
@@ -4,23 +4,22 @@ import io.github.dengchen2020.core.interceptor.BaseHandlerMethodInterceptor;
 import io.github.dengchen2020.core.security.principal.AnonymousAuthentication;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.security.Principal;
 import org.jspecify.annotations.NullMarked;
 import org.springframework.web.method.HandlerMethod;
 
-import java.security.Principal;
-
 /**
- * 频控拦截器
+ * 频控拦截器，仅适用于简单场景，仅作用于已登录用户
  *
  * @author xiaochen
  * @since 2025/5/15
  */
 @NullMarked
-public class FrequencyControlInterceptor extends BaseHandlerMethodInterceptor {
+public class SimpleFrequencyControlInterceptor extends BaseHandlerMethodInterceptor {
 
     private final FrequencyControlSupport frequencyControlSupport;
 
-    public FrequencyControlInterceptor(FrequencyControlSupport frequencyControlSupport) {
+    public SimpleFrequencyControlInterceptor(FrequencyControlSupport frequencyControlSupport) {
         this.frequencyControlSupport = frequencyControlSupport;
     }
 
@@ -30,10 +29,24 @@ public class FrequencyControlInterceptor extends BaseHandlerMethodInterceptor {
         if (control == null)
             control = handlerMethod.getBeanType().getAnnotation(FrequencyControl.class);
         if (control == null) return true;
-        Principal principal = request.getUserPrincipal();
-        if (principal == null || principal instanceof AnonymousAuthentication) return true;
-        long res = frequencyControlSupport.trigger(request.getRequestURI() + ":" + request.getMethod() + ":" + principal.getName(), control.qps(), control.qpm(), control.qpd());
+        if (shouldNotTrigger(request)) return true;
+        long res = frequencyControlSupport.trigger(getTriggerKey(request, handlerMethod), control.qps(), control.qpm(), control.qpd());
         if (res == 0) return true;
+        handleResult(control, res);
+        return true;
+    }
+
+    protected boolean shouldNotTrigger(HttpServletRequest request) {
+        Principal principal = request.getUserPrincipal();
+        return principal == null || principal instanceof AnonymousAuthentication;
+    }
+
+    protected String getTriggerKey(HttpServletRequest request, HandlerMethod handlerMethod) {
+        Principal principal = request.getUserPrincipal();
+        return request.getRequestURI() + ":" + request.getMethod() + ":" + principal.getName();
+    }
+
+    protected void handleResult(FrequencyControl control, long res) {
         if (res == 1) {
             throw new FrequencyControlException("触发秒级频次限制，请稍后再试", "qps", control.qps());
         } else if (res == 2) {
@@ -41,7 +54,6 @@ public class FrequencyControlInterceptor extends BaseHandlerMethodInterceptor {
         } else if (res == 3) {
             throw new FrequencyControlException("触发天级频次限制，请明天再试", "qpd", control.qpd());
         }
-        return true;
     }
 
 }
