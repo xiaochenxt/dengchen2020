8bc34f713d3058770a54cbacd9e0bc3fe7c6e899
xc <xc@>
2026-05-30 16:47:20 +0800

    优化

--- dc-spring-boot-starter-ratelimiter/src/main/java/io/github/dengchen2020/ratelimiter/AbstractRateLimiterInterceptor.java
+++ dc-spring-boot-starter-ratelimiter/src/main/java/io/github/dengchen2020/ratelimiter/AbstractRateLimiterInterceptor.java
@@ -13,7 +13,6 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.method.HandlerMethod;
 
 import java.security.Principal;
-import java.util.concurrent.TimeUnit;
 
 /**
  * 限流拦截器
@@ -22,38 +21,28 @@ import java.util.concurrent.TimeUnit;
  * @since 2024/8/3
  */
 @NullMarked
-public class RateLimiterInterceptor extends BaseHandlerMethodInterceptor {
-
-    private final RateLimiter secondRateLimiter;
-
-    private final RateLimiter minuteRateLimiter;
+public abstract class AbstractRateLimiterInterceptor extends BaseHandlerMethodInterceptor {
 
     private final String errorMsg;
 
-    public RateLimiterInterceptor(RateLimiter secondRateLimiter, RateLimiter minuteRateLimiter, String errorMsg) {
-        this.secondRateLimiter = secondRateLimiter;
-        this.minuteRateLimiter = minuteRateLimiter;
+    public AbstractRateLimiterInterceptor(String errorMsg) {
         this.errorMsg = errorMsg;
     }
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, HandlerMethod handlerMethod) {
-        RateLimit rateLimit = handlerMethod.getMethod().getAnnotation(RateLimit.class);
-        if (rateLimit == null) rateLimit = handlerMethod.getBeanType().getAnnotation(RateLimit.class);
-        if (rateLimit == null) return true;
+    protected String getLimitKey(HttpServletRequest request, RateLimit rateLimit, String methodName) {
         RateLimitStrategy strategy = rateLimit.strategy();
         String limitKey;
         switch (strategy) {
             case userAndUri -> {
                 Principal principal = request.getUserPrincipal();
                 if (principal == null || principal instanceof AnonymousAuthentication) {
-                    limitKey = RequestUtils.getRemoteAddr(request) + request.getRequestURI() + request.getMethod();
+                    limitKey = RequestUtils.getRemoteAddr(request) + ":" + methodName;
                 } else {
-                    limitKey = principal.getName() + request.getRequestURI() + request.getMethod();
+                    limitKey = principal.getName()+ ":" + methodName;
                 }
             }
             case ip -> limitKey = RequestUtils.getRemoteAddr(request);
-            case ipAndUri -> limitKey = RequestUtils.getRemoteAddr(request) + request.getRequestURI() + request.getMethod();
+            case ipAndUri -> limitKey = RequestUtils.getRemoteAddr(request) + ":" + methodName;
             case user -> {
                 Principal principal = request.getUserPrincipal();
                 if (principal == null || principal instanceof AnonymousAuthentication) {
@@ -62,16 +51,21 @@ public class RateLimiterInterceptor extends BaseHandlerMethodInterceptor {
                     limitKey = principal.getName();
                 }
             }
-            case uri -> limitKey = request.getRequestURI() + request.getMethod();
-            case null, default -> limitKey = RequestUtils.getRemoteAddr(request) + request.getRequestURI() + request.getMethod();
+            case uri -> limitKey = methodName;
+            case null, default -> limitKey = RequestUtils.getRemoteAddr(request) + ":" + methodName;
         }
-        RateLimiter rateLimiter;
-        if (rateLimit.timeUnit() == TimeUnit.MINUTES) {
-            rateLimiter = minuteRateLimiter;
-        } else {
-            rateLimiter = secondRateLimiter;
-        }
-        if (rateLimiter.limit(limitKey, rateLimit.value())) throw new RateLimitException(StringUtils.hasText(rateLimit.errorMsg()) ? rateLimit.errorMsg() : errorMsg);
+        return limitKey;
+    }
+
+    protected abstract boolean limit(RateLimit rateLimit, String limitKey);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, HandlerMethod handlerMethod) {
+        RateLimit rateLimit = handlerMethod.getMethod().getAnnotation(RateLimit.class);
+        if (rateLimit == null) rateLimit = handlerMethod.getBeanType().getAnnotation(RateLimit.class);
+        if (rateLimit == null) return true;
+        String limitKey = getLimitKey(request, rateLimit, handlerMethod.toString());
+        if (limit(rateLimit, limitKey)) throw new RateLimitException(StringUtils.hasText(rateLimit.errorMsg()) ? rateLimit.errorMsg() : errorMsg);
         return true;
     }
 }
