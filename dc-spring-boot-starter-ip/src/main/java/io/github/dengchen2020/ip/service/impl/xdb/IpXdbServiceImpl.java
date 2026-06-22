d09768754212eabf4cf726a174c4e4e837ffb715
xc <xc@>
2026-06-22 09:07:23 +0800

    优化

--- dc-spring-boot-starter-ip/src/main/java/io/github/dengchen2020/ip/service/impl/xdb/IpXdbServiceImpl.java
+++ dc-spring-boot-starter-ip/src/main/java/io/github/dengchen2020/ip/service/impl/xdb/IpXdbServiceImpl.java
@@ -30,56 +30,38 @@ public class IpXdbServiceImpl implements IpService, DisposableBean {
 
     private static final Logger log = LoggerFactory.getLogger(IpXdbServiceImpl.class);
 
-    private Searcher searcherIpv4;
+    private final Searcher searcherIpv4;
 
-    @Nullable private Searcher searcherIpv6;
+    @Nullable protected final Searcher searcherIpv6;
 
     public IpXdbServiceImpl(String ipv4DataPath,@Nullable String ipv6DataPath, boolean verify) {
-        loadData(ipv4DataPath, ipv6DataPath, verify);
-    }
-
-    public synchronized void loadData(String ipv4DataPath,@Nullable String ipv6DataPath, boolean verify) {
         File ipv4File = new File(ipv4DataPath);
         if (ipv4File.isFile() && ipv4File.exists()) {
             try {
-                Searcher newSearcherIpv4 = Searcher.newWithBuffer(Version.IPv4, Searcher.loadContentFromFile(ipv4File));
+                searcherIpv4 = Searcher.newWithBuffer(Version.IPv4, Searcher.loadContentFromFile(ipv4File));
                 if (verify) Searcher.verifyFromFile(ipv4File);
                 log.info("ipv4数据包加载完成，占用{}MB", DataSize.ofBytes(ipv4File.length()).toMegabytes());
-                var oldSearcherIpv4 = searcherIpv4;
-                searcherIpv4 = newSearcherIpv4;
-                if (oldSearcherIpv4 != null) {
-                    try {
-                        oldSearcherIpv4.close();
-                    } catch (IOException e) {
-                        log.error("关闭旧的ipv4查询对象失败：", e);
-                    }
-                }
             } catch (IOException | XdbException e) {
                 throw new IllegalArgumentException("ipv4数据包加载失败", e);
             }
         } else {
             throw new IllegalArgumentException("未找到ipv4数据包，请将其放置在" + ipv4File.getAbsolutePath());
         }
-        if (ipv6DataPath == null) return;
+        if (ipv6DataPath == null) {
+            searcherIpv6 = null;
+            return;
+        }
         File ipv6File = new File(ipv6DataPath);
         if (ipv6File.isFile() && ipv6File.exists()) {
             try {
-                Searcher newSearcherIpv6 = Searcher.newWithBuffer(Version.IPv6, Searcher.loadContentFromFile(ipv6File));
+                searcherIpv6 = Searcher.newWithBuffer(Version.IPv6, Searcher.loadContentFromFile(ipv6File));
                 if (verify) Searcher.verifyFromFile(ipv6File);
                 log.info("ipv6数据包加载完成，占用{}MB", DataSize.ofBytes(ipv6File.length()).toMegabytes());
-                var oldSearcherIpv6 = searcherIpv6;
-                searcherIpv6 = newSearcherIpv6;
-                if (oldSearcherIpv6 != null) {
-                    try {
-                        oldSearcherIpv6.close();
-                    } catch (IOException e) {
-                        log.error("关闭旧的ipv6查询对象失败：", e);
-                    }
-                }
             } catch (IOException | XdbException e) {
                 throw new IllegalArgumentException("ipv6数据包加载失败", e);
             }
         } else {
+            searcherIpv6 = null;
             if (log.isDebugEnabled()) log.debug("未找到ipv6数据包，如需查询ipv6信息，请将其放置在{}，如不需要可忽略", ipv6File.getAbsolutePath());
         }
     }
@@ -88,29 +70,27 @@ public class IpXdbServiceImpl implements IpService, DisposableBean {
     public IpInfo getInfo(String ip) {
         if (!StringUtils.hasText(ip)) return defaultInfo(ip);
         try {
-            String[] ipInfo;
+            String[] ipData;
             var data = Util.parseIP(ip);
             if (data.length == 16) {
                 if (searcherIpv6 == null) {
                     log.error("未加载ipv6数据包，无法查询ipv6信息");
                     return defaultInfo(ip);
                 }
-                ipInfo = searcherIpv6.search(data).split("\\|");
+                ipData = searcherIpv6.search(data).split("\\|");
             } else {
-                ipInfo = searcherIpv4.search(data).split("\\|");
+                ipData = searcherIpv4.search(data).split("\\|");
             }
-            if (ipInfo.length > 0) {
-                return new IpInfo(ip, getValue(ipInfo, 0), getValue(ipInfo, 1),
-                        getValue(ipInfo, 2), getValue(ipInfo, 3), getValue(ipInfo, 4),
-                        getValue(ipInfo, 5), getValue(ipInfo, 6), getValue(ipInfo, 7),
-                        getValue(ipInfo, 8), getValue(ipInfo, 9), getValue(ipInfo, 10),
-                        getValue(ipInfo, 11), getValue(ipInfo, 12), getValue(ipInfo, 13))
-                        ;
-            }
+            if (ipData.length > 0) return convert(ip, ipData);
         } catch (Exception e) {
             log.error("获取ip信息失败：{}", e.toString());
         }
         return defaultInfo(ip);
+    }
+
+    protected IpInfo convert(String ip, String[] data) {
+        return new IpInfo(ip, getValue(data, 1), getValue(data, 2),
+                getValue(data, 3), getValue(data, 4), getValue(data, 11));
     }
 
     @Override
