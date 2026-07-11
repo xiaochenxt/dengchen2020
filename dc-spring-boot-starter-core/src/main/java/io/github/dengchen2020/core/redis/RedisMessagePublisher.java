bfb602fd97876efa70815a5caf2baa4b0be0440d
xc <xc@>
2026-07-11 16:23:18 +0800

    移除延迟发布，容易误用

--- dc-spring-boot-starter-core/src/main/java/io/github/dengchen2020/core/redis/RedisMessagePublisher.java
+++ dc-spring-boot-starter-core/src/main/java/io/github/dengchen2020/core/redis/RedisMessagePublisher.java
@@ -72,51 +72,6 @@ public class RedisMessagePublisher {
         }
     }
 
-    /**
-     * 延迟发布消息
-     * <p>默认使用Json序列化</p>
-     *
-     * @param channelName 频道名称
-     * @param message     消息
-     * @param delay 延迟时长
-     */
-    public void publish(String channelName, Object message, Duration delay) {
-        byte[] bytes = serializer.serialize(message);
-        convertAndSend(channelName, bytes, delay);
-    }
-
-    /**
-     * 延迟发布字符串消息
-     *
-     * @param channelName 频道名称
-     * @param message     消息
-     * @param delay 延迟时长
-     */
-    public void publish(String channelName, String message, Duration delay) {
-        convertAndSend(channelName, message.getBytes(StandardCharsets.UTF_8), delay);
-    }
-
-    /**
-     * 延迟发布字节数组消息
-     *
-     * @param channelName 频道名称
-     * @param message     消息
-     * @param delay 延迟时长
-     */
-    public void publish(String channelName, byte[] message, Duration delay) {
-        convertAndSend(channelName, message, delay);
-    }
-
-    private void convertAndSend(String channelName, byte[] message, Duration delay){
-        try {
-            reactiveRedisTemplate.convertAndSend(channelName, message).delaySubscription(delay).retryWhen(Retry.backoff(3, Duration.ofSeconds(1))).subscribe(count -> {
-                if (log.isDebugEnabled()) logMessage(channelName, message, count);
-            });
-        } catch (Exception e) {
-            log.error("convertAndSend error", e);
-        }
-    }
-
     private void logMessage(String channelName, byte[] message, Long count) {
         log.debug("发布消息 {} 到通道 {}，收到消息的客户端数量：{}", new String(message), channelName, count);
     }
