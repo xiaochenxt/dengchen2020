7549df917001e5fecd94d9a9369aacc32e26ef7f
xc <xc@>
2026-07-17 11:36:20 +0800

    被限流时返回时间级别以实现允许客户端安全重试

--- dc-spring-boot-starter-ratelimiter/src/main/java/io/github/dengchen2020/ratelimiter/exception/RateLimitException.java
+++ dc-spring-boot-starter-ratelimiter/src/main/java/io/github/dengchen2020/ratelimiter/exception/RateLimitException.java
@@ -2,13 +2,24 @@ package io.github.dengchen2020.ratelimiter.exception;
 
 import io.github.dengchen2020.core.exception.ViewToastException;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * 被限流异常
  * @author xiaochen
  * @since 2024/7/1
  */
 public class RateLimitException extends ViewToastException {
-    public RateLimitException(String message) {
+    private final TimeUnit timeUnit;
+    public RateLimitException(String message, TimeUnit timeUnit) {
         super(message, ViewToastException.CODE, null, false);
+        this.timeUnit = timeUnit;
+    }
+
+    /**
+     * 限流时间级别，可通过判断如果为秒级返回http429错误来允许客户端安全重试
+     */
+    public TimeUnit getTimeUnit() {
+        return timeUnit;
     }
 }
