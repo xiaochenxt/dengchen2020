bc26fdfe6f45c61ab7a311addaea3894886a1733
xc <xc@>
2026-07-23 09:54:03 +0800

    为超时和缓冲区大小提供配置项

--- dc-spring-boot-starter-websocket/src/main/java/io/github/dengchen2020/websocket/config/SpringWebSocketAutoConfiguration.java
+++ dc-spring-boot-starter-websocket/src/main/java/io/github/dengchen2020/websocket/config/SpringWebSocketAutoConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.util.ClassUtils;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -75,6 +76,7 @@ public final class SpringWebSocketAutoConfiguration implements WebSocketConfigur
     /**
      * 配置websocket容器
      */
+    @Lazy(false)
     @ConditionalOnMissingBean
     @Bean
     ServletServerContainerFactoryBean serverContainerFactoryBean() {
