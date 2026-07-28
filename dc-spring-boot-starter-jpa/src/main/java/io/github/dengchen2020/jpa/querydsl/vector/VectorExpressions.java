9dff8c57f94bb2c568c57454b656b99e13f3d418
xc <xc@>
2026-07-28 16:50:24 +0800

    新增向量表达式

---
+++ dc-spring-boot-starter-jpa/src/main/java/io/github/dengchen2020/jpa/querydsl/vector/VectorExpressions.java
@@ -0,0 +1,127 @@
+package io.github.dengchen2020.jpa.querydsl.vector;
+
+import com.querydsl.core.types.dsl.ArrayExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.core.types.dsl.SimpleTemplate;
+import org.jspecify.annotations.NullMarked;
+
+/**
+ * Vector表达式
+ * @author xiaochen
+ * @since 2025/12/29
+ */
+@NullMarked
+public final class VectorExpressions {
+
+    private VectorExpressions(){}
+
+    /**
+     * sql：{@code a <=> b}，余弦距离
+     * @param expr 要匹配的向量
+     * @param vector 要匹配的向量
+     * @return {@link NumberExpression<Double>}
+     */
+    public static NumberExpression<Double> cosineDistance(ArrayExpression<float[], Float> expr, float[] vector){
+        return Expressions.numberTemplate(Double.class,"cosine_distance({0},{1})", expr, vector);
+    }
+
+    /**
+     * sql：{@code a <-> b}，Euclidean distance
+     * @param expr 要匹配的向量
+     * @param vector 要匹配的向量
+     * @return {@link NumberExpression<Double>}
+     */
+    public static NumberExpression<Double> l2Distance(ArrayExpression<float[], Float> expr, float[] vector){
+        return Expressions.numberTemplate(Double.class,"l2_distance({0},{1})", expr, vector);
+    }
+
+    /**
+     * sql：{@code (a <-> b)^2}，squared Euclidean distance
+     * @param expr 要匹配的向量
+     * @param vector 要匹配的向量
+     * @return {@link NumberExpression<Double>}
+     */
+    public static NumberExpression<Double> l2SquaredDistance(ArrayExpression<float[], Float> expr, float[] vector){
+        return Expressions.numberTemplate(Double.class,"l2_squared_distance({0},{1})", expr, vector);
+    }
+
+    /**
+     * sql：{@code l1_distance(vector, vector)}，taxicab distance
+     * @param expr 要匹配的向量
+     * @param vector 要匹配的向量
+     * @return {@link NumberExpression<Double>}
+     */
+    public static NumberExpression<Double> l1Distance(ArrayExpression<float[], Float> expr, float[] vector){
+        return Expressions.numberTemplate(Double.class,"l1_distance({0},{1})", expr, vector);
+    }
+
+    /**
+     * sql：{@code (a <#> b) *-1}，inner product
+     * @param expr 要匹配的向量
+     * @param vector 要匹配的向量
+     * @return {@link NumberExpression<Double>}
+     */
+    public static NumberExpression<Double> innerProduct(ArrayExpression<float[], Float> expr, float[] vector){
+        return Expressions.numberTemplate(Double.class,"inner_product({0},{1})", expr, vector);
+    }
+
+    /**
+     * sql：{@code a <#> b}，negative inner product
+     * @param expr 要匹配的向量
+     * @param vector 要匹配的向量
+     * @return {@link NumberExpression<Double>}
+     */
+    public static NumberExpression<Double> negativeInnerProduct(ArrayExpression<float[], Float> expr, float[] vector){
+        return Expressions.numberTemplate(Double.class,"negative_inner_product({0},{1})", expr, vector);
+    }
+
+    /**
+     * sql：{@code vector_dims(vector)}, number of dimensions
+     * @param expr 要匹配的向量
+     * @return {@link NumberExpression<Integer>}
+     */
+    public static NumberExpression<Integer> vectorDims(ArrayExpression<float[], Float> expr){
+        return Expressions.numberTemplate(Integer.class,"vector_dims({0})", expr);
+    }
+
+    /**
+     * sql：{@code vector_norm(vector)}，Euclidean norm
+     * @param expr 要匹配的向量
+     * @return {@link NumberExpression<Double>}
+     */
+    public static NumberExpression<Double> vectorNorm(ArrayExpression<float[], Float> expr){
+        return Expressions.numberTemplate(Double.class,"vector_norm({0})", expr);
+    }
+
+    /**
+     * sql：{@code binary_quantize(vector)}，binary quantize
+     * @param expr 要匹配的向量
+     * @return {@link SimpleTemplate}
+     */
+    public static SimpleTemplate<byte[]> binaryQuantize(ArrayExpression<float[], Float> expr){
+        return Expressions.template(byte[].class,"binary_quantize({0})", expr);
+    }
+
+    /**
+     * sql：{@code subvector(vector, offset, length)}，subvector
+     *
+     * @param expr   要匹配的向量
+     * @param offset 偏移量
+     * @param length 长度
+     * @return {@link SimpleTemplate}
+     */
+    public static SimpleTemplate<float[]> subvector(ArrayExpression<float[], Float> expr, int offset, int length){
+        return Expressions.template(float[].class,"subvector({0},{1},{2})", expr, offset, length);
+    }
+
+    /**
+     * sql：{@code l2_normalize(vector)}，l2 normalize
+     * @param expr 要匹配的向量
+     * @return {@link SimpleTemplate}
+     */
+    public static SimpleTemplate<float[]> l2Normalize(ArrayExpression<float[], Float> expr){
+        return Expressions.template(float[].class,"l2_normalize({0})", expr);
+    }
+
+}
